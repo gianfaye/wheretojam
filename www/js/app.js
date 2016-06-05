43c58996886683bc5c25bfd2477e807f555d1626
Angela <angelapaguirigan@gmail.com>
2016-06-06 02:14:48 +0800

    Design fixes for header and arrow icon

--- www/js/app.js
+++ www/js/app.js
@@ -1,12 +1,10 @@
 var Header = React.createClass({
     render: function () {
         return (
-            <div className="header-container">
-                <header className="bar bar-nav">
-                    <a href="#" className={"icon icon-left-nav pull-left" + (this.props.back==="true"?"":" hidden")}></a>
-                    <h1 className="title">{this.props.text}</h1>
-                </header>
-            </div>
+            <header className="bar bar-nav">
+                <a href="#" className={"icon icon-left-nav pull-left" + (this.props.back==="true"?"":" hidden")}></a>
+                <h1 className="title">{this.props.text}</h1>
+            </header>
         );
     }
 });
